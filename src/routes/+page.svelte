1a844fedf48be2862b759b1a17ac46f477f51103
Raghad <raghad11ali@gmail.com>
2024-08-08 15:50:56 +0300

    buttom

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import Button from "$lib/components/ui/button/button.svelte";
+</script>
+<div class="flex justify-center items-center min-h-screen  ">
+<Button variant="ghost">
+hhh
+</Button>
+</div>
