9f68bce93eb03bc0d973490504c2311a3e80308a
Raghad <raghad11ali@gmail.com>
2024-08-10 16:19:26 +0300

    Theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script>
 import Button from "$lib/components/ui/button/button.svelte";
+import ThemeSwitch from "$lib/components/ThemeSwitch.svelte";
 </script>
-<div class="flex justify-center items-center min-h-screen  ">
-<Button variant="ghost">
-hhh
-</Button>
+<div class="flex justify-center items-center min-h-screen text-6xl text-primary">
+    jekki wirkd
+    <ThemeSwitch/>
+    <div class="bg-secondary border-2 h-20 w-20"></div>
 </div>
