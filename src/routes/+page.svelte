3bcb243533e0c42fa5924d3db55bfb04fedc664e
Raghad <raghad11ali@gmail.com>
2024-08-11 12:41:32 +0300

    1pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,6 @@
-<script>
-import Button from "$lib/components/ui/button/button.svelte";
-import ThemeSwitch from "$lib/components/ThemeSwitch.svelte";
+<script lang="ts">
+    import {Button} from "$lib/components/ui/button"
 </script>
-<div class="flex justify-center items-center min-h-screen text-6xl text-primary">
-    jekki wirkd
-    <ThemeSwitch/>
-    <div class="bg-secondary border-2 h-20 w-20"></div>
-</div>
+<Button href="\contact">
+    click me to go to conteact page
+</Button>
