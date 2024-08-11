1bf1d99acc4cd42ac6f120ac966a93a6a08450ab
Raghad <raghad11ali@gmail.com>
2024-08-11 14:15:37 +0300

    nav1/2 light purple dark blue

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,29 @@
+<script>
+import {Button} from "$lib/components/ui/button";
+import ThemeSwitch from "$lib/components/ThemeSwitch.svelte";
+</script>
+
+<nav class="flex justify-between items-baseline w-[100dvw] bg-secondary text-secondary-foreground py-8 px-4 ">
+    <div class="flex items-center gap-2 ">
+        <div class="bg-primary h-5 w-5 me-1"/>
+        <div>
+        <span class="font-bold text-2xl">Raghad Alsuwayhi</span>
+        <span class="text-xl">/ Web Developer</span>
+        </div>
+    </div>
+<div class="uppercase">
+    <Button href="./" variant="link">
+        About me
+    </Button>
+    <Button href="./resume" variant="link">
+    Resume
+    </Button>
+    <Button href="./projects"variant="link">
+        Project
+    </Button>
+    <Button href="./contact" variant="link">
+        Contect
+    </Button>
+    <ThemeSwitch/>
+</div>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,1 @@
-<script lang="ts">
-    import {Button} from "$lib/components/ui/button"
-</script>
-<Button href="\contact">
-    click me to go to conteact page
-</Button>
+ijoi;
