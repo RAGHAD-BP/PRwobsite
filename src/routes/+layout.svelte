c283380836197b56ea8139a37c273289ba216c6a
Raghad <raghad11ali@gmail.com>
2024-11-16 23:12:17 +0300

    try push

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
     import "../app.css";
     import { ModeWatcher } from "mode-watcher";
     import Navbar from "$lib/components/Navbar.svelte";
+
 </script>
 <!-- svelte-ignore missing-declaration -->
 <Navbar/>
