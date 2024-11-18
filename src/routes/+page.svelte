429a7488f47fea9e9c68ab31af207e5586257973
Raghad <raghad11ali@gmail.com>
2024-11-18 22:34:59 +0300

    start card1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
     import Project from "./Project/+page.svelte";
     import Contact from "./Contect/+page.svelte";
-    
 </script>
 <div>--------About me----------</div>
 <div class="sm:block flex ">
+<div class="border-primary border-2 flex flex-col h-[410px] w-[320px] ">
+    <img src="/bcanav.png " class="h-[320px] w-[320px]" alt="">
+    <div class="font-bold">Raghad Alsuwayhi</div>
+    <div>programmer</div>
+    <div class="bg-card">
+        jjjh</div>
+</div>
 
-    <div class="text-3xl">
-    Hello i'm a 
-    bigenner web delovbor
+    <div class="text-2xl">
+    Hello i'm a web delovbor
 </div>
 </div>
 <div>
