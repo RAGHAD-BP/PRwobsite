ffce073cd977340a369c0abc4241411a5d6e18a2
Raghad <raghad11ali@gmail.com>
2024-11-19 01:19:10 +0300

    start project page

--- src/routes/Project/+page.svelte
+++ src/routes/Project/+page.svelte
@@ -1,1 +1,9 @@
+<script>
+</script>
+
 ----------project---------
+<!--<div class="flex  px-5 m-2 mx-2">-->
+<div class="h-fit w-fit border-2 rounded-2xl border-primary px-5 m-5">first project</div>
+<div class="h-fit w-fit border-2 rounded-2xl border-primary px-5 m-5">seco project</div>
+<div class="h-fit w-fit border-2 rounded-2xl border-primary px-5 m-5">their project</div>
+<!--</div>-->
