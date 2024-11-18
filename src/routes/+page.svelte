84e11a33f4b4038e76564c842b9cb14dd9987a6b
Raghad <raghad11ali@gmail.com>
2024-11-18 20:04:07 +0300

    show the div between the pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,17 @@
     import Contact from "./Contect/+page.svelte";
     
 </script>
+<div>--------About me----------</div>
 <div class="sm:block flex ">
-    <div class="text-4xl">
+
+    <div class="text-3xl">
     Hello i'm a 
     bigenner web delovbor
 </div>
 </div>
+<div>
 <Project/>
+</div>
+<div>
 <Contact/>
+</div>
--- src/routes/Project/+page.svelte
+++ src/routes/Project/+page.svelte
@@ -1,2 +1,1 @@
-fjfjf
-dkdk
+----------project---------
