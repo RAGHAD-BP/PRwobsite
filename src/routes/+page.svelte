931dffd0263d0644bff9ab3195e8dd377674d29a
Raghad <raghad11ali@gmail.com>
2024-11-19 21:14:19 +0300

    start page not done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,26 +4,24 @@
     import {Button} from "$lib/components/ui/button";
 </script>
 <div>--------About me----------</div>
-<div class="block pr-5 pl-5  flex justify-items-end justify-center ">
-<div class="shadow-xl  flex flex-col  w-[320px] mr-5 h-fit">
-    <img src="/bcanav.png " class="h-[320px] w-[320px]" >
-    <div class="font-bold items-center">Raghad Alsuwayhi</div>
-    <div>programmer</div>
-    <div class="bg-card h-[30px]">
-        jjjh</div>
-</div>
-    <div class="font-medium w-[250px] text-center ">
+<div class="block pr-5 pl-5  flex justify-items-end justify-center m-5 sm:items-start">
+    <img src="/bcanav.png " class="h-[320px] w-[320px] rounded-full shadow-primary shadow-[0_10px_80px_-15px_rgba(0,0,0,3)] mx-7" >
+
+    <h1 class="font-medium w-[250px] text-center mx-7">
         Hello i'm Raghad a student in
         umm alqra universite my migor is
         compouter sciens and i a web delovbor
         and programmer
-        <div class="m-5">
+
+        <div class="my-5  items-center ">
         <Button>project</Button>
         <Button>contect</Button>
         </div>
-    </div>
+
+    </h1>
 
 </div>
+
 
 <div>
 <Project/>
