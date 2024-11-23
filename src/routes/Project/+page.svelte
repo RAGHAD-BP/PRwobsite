17d790f35d31d3fcc4f7fcf7609535effd0b065f
Raghad <raghad11ali@gmail.com>
2024-11-23 17:58:21 +0300

    gap

--- src/routes/Project/+page.svelte
+++ src/routes/Project/+page.svelte
@@ -4,23 +4,23 @@ import {Button} from "$lib/components/ui/button";
 </script>
 
 
-<div class="text-3xl font-bold text-center ">Projects</div>
+<div class="text-3xl font-bold text-center p-10">Projects</div>
 
-<div class="flex flex-col flex-row ">
+<div class="flex flex-col gap-16 items-center p-10 m-5 ">
 
 
-<div class="h-fit w-fit rounded-lg p-5 m-5 shadow-primary shadow-[0px_5px_80px_-5px_rgba(0,0,0,3)]">
+<div class="rounded-lg p-5 m-5 shadow-primary shadow-[0px_5px_80px_-5px_rgba(0,0,0,3)]">
     <div class="text-2xl font-bold">1</div>
-<div class="flex">the project is calcleter it is the first project have build every progreammer    <div><img class=" max-w-[200px] max-h-[200px] sm:w-h-[100px] w-[100px]  " src="/calc.png"></div></div>
+<div class="flex gap-4">the project is calcleter it is the first project have build every progreammer  .   <div><img class=" max-w-[200px] max-h-[200px] sm:w-h-[100px] w-[100px] " src="/calc.png"></div></div>
 <Button class="m-2">try it</Button><Button class="m-1">see the code</Button></div>
 
 
-    <div class="h-fit w-fit rounded-lg p-5 m-5 shadow-primary shadow-[0px_5px_80px_-5px_rgba(0,0,0,3)]"><div class="text-2xl font-bold">2</div><div class="flex">the project is to do list you can make task and when you done it you can make it done or delet it and you have all task and task today<div><img class=" max-w-[200px] max-h-[200px] sm:w-h-[100px] w-[100px]" src="/todolist.jpg"></div>
+    <div class="h-fit w-fit rounded-lg p-5 m-5 shadow-primary shadow-[0px_5px_80px_-5px_rgba(0,0,0,3)] sm:h-fit w-fit"><div class="text-2xl font-bold">2</div><div class="flex gap-4">the project is to do list you can make task and when you done it you can make it done or delet it and you have all task and task today <div><img class=" max-w-[200px] max-h-[200px] sm:w-h-[100px] w-[100px] " src="/todolist.jpg"></div>
 </div><Button class="m-2">try it</Button><Button class="m-1">see the code</Button>
 </div>
 
 
-<div class="h-fit w-fit rounded-lg p-5 m-5 shadow-primary shadow-[0px_5px_80px_-5px_rgba(0,0,0,3)]"><div class="text-2xl font-bold">3</div><div class="flex">this is a GPA project it calcte you the GPA of the smester only and the all smester<div><img class=" max-w-[200px] max-h-[200px] sm:w-h-[100px] w-[100px]" src="/favicon.png"> </div></div><Button class="m-2">try it</Button><Button class="m-1">see the code</Button></div>
+<div class="h-fit w-fit rounded-lg p-5 m-5 shadow-primary shadow-[0px_5px_80px_-5px_rgba(0,0,0,3)]"><div class="text-2xl font-bold">3</div><div class="flex gap-4">this is a GPA project it calcte you the GPA of the smester only and the all smester<div><img class=" max-w-[200px] max-h-[200px] sm:w-h-[100px] w-[100px]" src="/favicon.png"> </div></div><Button class="m-2">try it</Button><Button class="m-1">see the code</Button></div>
 
 
 </div>
