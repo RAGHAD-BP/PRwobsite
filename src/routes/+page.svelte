697ad1d5f03ada5b832c9af7d407cbc716aacb00
Raghad <raghad11ali@gmail.com>
2024-11-30 18:52:11 +0300

    pages done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
     import {Button} from "$lib/components/ui/button";
     import Footer from "$lib/components/Footer.svelte";
 </script>
-<div class="text-3xl font-bold text-center pt-32 py-10">Rusme</div>
+<div class="text-3xl font-bold text-center pt-32 py-10">About me</div>
 
 <div class="block flex flex-col pr-5 pl-5 items-center lg:ml-72 sm:flex-row gap-5 ">
     <img src="bcanav.png" class="h-[320px] w-[320px] items-center rounded-full shadow-primary shadow-[0px_5px_80px_-5px_rgba(0,0,0,3)] mx-7" >
@@ -13,16 +13,16 @@
         Hello, I am <h class="text-primary">Raghad</h>, a computer science student at um Al-Qura University, and I am interested in web development
 
         <div class="my-5 ml-7 items-center flex flex-row">
-        <Button class="m-2" href="Project">project</Button>
-        <Button class="m-1" href="Contect">contect</Button>
+        <Button class="m-2"><a href="#project">project</a></Button>
+        <Button class="m-1"><a href="#contect">contect</a></Button>
         </div>
 
     </h1>
 
 </div>
-
-
+<div id="project">
     <Project/>
-
-
+</div>
+<div id="contect">
     <Contect/>
+</div>
