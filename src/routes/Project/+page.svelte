29811414df76fe2c34b23cd559009cb35d64c6e9
RAGHAD-BP <173504653+RAGHAD-BP@users.noreply.github.com>
2026-04-03 19:50:30 +0300

    Add new project (Eid games)

--- src/routes/Project/+page.svelte
+++ src/routes/Project/+page.svelte
@@ -6,12 +6,17 @@ import {Button} from "$lib/components/ui/button";
 <div class="text-3xl font-bold text-center pt-32 py-10 ">Projects</div>
 
 <div class="flex flex-col font-normal items-center p-10">
-<div class="rounded-lg min-w-[18rem]  h-fit p-5 m-5 border ">
+<div class="rounded-lg min-w-[18rem]  h-fit p-5 m-5 border sm:h-fit w-fit">
 <div class="flex gap-4"> This is the first project, which is a calculator that you can use to calculate multiplication, division, addition, subtraction, remainders, and other operations...  <div><img class=" max-w-[200px] max-h-[200px] sm:w-h-[100px] w-[100px] " src="/calc.png"></div></div>
 <Button class="m-2" href="https://resilient-valkyrie-7c6c82.netlify.app/">try it</Button><Button class="m-1" href="https://github.com/RAGHAD-BP/calculator">see the code</Button></div>
+
 <div class="h-fit min-w-[18rem] rounded-lg p-5 m-5 border sm:h-fit w-fit "><div class="flex gap-4">
 This project is a simple daily to‑do list where you can add tasks by time and day, move completed tasks to a separate section, and delete anything you don’t need. Your tasks are saved automatically on the website, so even if you close or refresh the page, nothing gets lost.<div><img class=" max-w-[200px] max-h-[200px] sm:w-h-[100px] w-[100px] " src="/todolist.jpg"></div>
-            </div><Button class="m-2" href="https://to-dolist-rust.vercel.app/">try it</Button><Button class="m-1" href="https://github.com/RAGHAD-BP/to-dolist">see the code</Button>
-            </div>
+</div><Button class="m-2" href="https://to-dolist-rust.vercel.app/">try it</Button><Button class="m-1" href="https://github.com/RAGHAD-BP/to-dolist">see the code</Button>
+</div>
 
+<div class="h-fit min-w-[18rem] rounded-lg p-5 m-5 border sm:h-fit w-fit "><div class="flex gap-4">
+This project offers a collection of fun games designed for families and friends to enjoy together during Eid. It brings laughter, friendly challenges, and joyful moments in a festive and playful atmosphere.<div><img class=" max-w-[200px] max-h-[200px] sm:w-h-[100px] w-[100px] " src="/EidGame.png"></div>
+</div><Button class="m-2" href="https://eidgamess.netlify.app/">try it</Button><Button class="m-1" href="https://github.com/RAGHAD-BP/EidGames/tree/main">see the code</Button>
+</div>
 </div>
